a2e0c1dad0a422741930a13e0934461637d6dc79
cdroot01 <cdroot01@gmail.com>
2024-06-05 21:32:02 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div class="navbar">
+      <RouterLink class="logo" to="/">Pet Social</RouterLink>
+     
+      <div class="navbar-action">
+        <RouterLink to="/login">Login</RouterLink>
+        |
+        <RouterLink to="/register">Register</RouterLink>
+      </div>
+    </div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
+  background: rgb(199, 199, 242);
+}
+
+.navbar-action {
+  margin-left: auto;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from "vue";
+const dataLogin = ref({
+    email: "",
+    password: ""
+})
+
+const login = async () => {
+    try {
+        await fetch("http://localhost:8000/api/auth/login", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dataLogin.value),
+        }).then(async res => {
+            const data = await res.json()
+            console.log(data)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Login</h1>
+
+        <div>
+            <div>
+                <label>Email</label>
+                <input type="text" v-model="dataLogin.email">
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" v-model="dataLogin.password">
+            </div>
+        </div>
+        <button @click="login">Login</button>
+    </div>
+</template>
